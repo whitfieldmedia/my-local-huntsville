77f78bb576fd1444dd8859386d1d75858b4bd3da
Spencer <backmanspencer99@gmail.com>
2019-06-20 09:02:33 -0500

    added padding top and bottom to pages to fit banner add

--- src/Gallery.js
+++ src/Gallery.js
@@ -85,6 +85,7 @@ class Gallery extends React.Component {
                 <h1 className="gallery-header"> Huntsville Alabama Photo Gallery </h1>
                 <p className="gallery-thanks"> A big shoutout to Ashley Davis for the awesome pictures! </p>
                 <GridGallery id="grid-gallery" images={this.state.images} enableImageSelection={false} />
+                <div className="gallery-bottom-spacer"></div>
             </div>
         )
     }
