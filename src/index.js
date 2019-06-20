5819b30be5aed2916ade03f6a4ffb4f7f4ecdce6
Spencer <backmanspencer99@gmail.com>
2019-06-20 10:04:53 -0500

    added error catching script to top of public html file

--- src/index.js
+++ src/index.js
@@ -5,33 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux';
 
-window.onerror = function (message, url, lineNo, colNo, error) {
-
-  console.log(arguments);
-
-  let container = document.createElement('div');
-
-  container.style.color = 'red';
-  container.style.position = 'fixed';
-  container.style.background = '#eee';
-  container.style.padding = '2em';
-  container.style.top = '1em';
-  container.style.left = '1em';
-
-  let msg = document.createElement('pre');
-  msg.innerText = [
-     'Message: ' + message,
-     'URL: ' + url,
-     'Line: ' + lineNo,
-     'Column: ' + colNo,
-     'Stack: ' + (error && error.stack)
-  ].join('\n');
-
-  container.appendChild(msg);
-
-  document.body.appendChild(container);
-};
-
 ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
