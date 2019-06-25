5a7f5b81952d3f6453ac38b6a496da3960f55778
Spencer <backmanspencer99@gmail.com>
2019-06-25 16:11:21 -0500

    cleaning up files

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import Poppy from './bars/Poppy';
-import ArtTrail from './activities/ArtTrail';
-import Bullet from './activities/Bullet';
-import Gardens from './activities/Gardens';
-import Bar805 from './bars/Bar805';
-import Brickhouse from './bars/Brickhouse';
-import Btr from './bars/Btr';
-import Otbx from './bars/Otbx';
-import Straight from './bars/Straight';
-import Yellowhammer from './bars/Yellowhammer';
-import Boot from './restaurants/Boot';
-import BrokenEgg from './restaurants/BrokenEgg';
 import { Link } from 'react-router-dom';
 import BackgroundSlideshow from 'react-background-slideshow';
 import News from './News';
@@ -60,43 +48,6 @@ class Home extends React.Component{
       })
     }
   }
-  showRestaurants = () => {
-    var places = [
-      <Yellowhammer key="1"/>, <Brickhouse key="2"/>, <Btr key="3"/>, <Straight key="4"/>, <Boot key="5"/>, <BrokenEgg key="6"/>, <Poppy key="7"/>, <Otbx key="8"/>, <Bar805 key="9"/>
-    ];
-    var a = [0,1,2,3,4,5,6,7,8];
-    var n;
-    var r = []
-    for (n=0; n < 4; ++n) {
-      var i = Math.floor((Math.random() * (9-n)));
-      r.push(a[i]);
-      a[i] = a[8-n];
-    }
-    var first = r[0]
-    var second = r[1]
-    var third = r[2]
-    var fourth = r[3]
-    let width = window.innerWidth;
-    if(width <= 1250) {
-      return [places[first], places[second]]
-    }else if (width <= 1650) {
-      return [places[first], places[second], places[third]]
-    } else {
-      return [
-        places[first], places[second], places[third], places[fourth]
-      ]
-    } 
-  }
-
-  showThings = () => {
-    let width = window.innerWidth;
-    if(width <= 1250) {
-      return [ <Gardens key="Botanical_Gardens"/>, <Bullet key="Bullet & Barrel" /> ]
-    } else if(width <= 1650) {
-      return [<Gardens key="Botanical_Gardens"/>, <Bullet key="Bullet & Barrel" />, <ArtTrail key="Secret_Art_Trail" />]
-    }
-  }
-
   render() {
     return (
       <div>
