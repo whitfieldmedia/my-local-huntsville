afc42a7409b21791cde0867fa81feaa633acb0e3
Spencer <backmanspencer99@gmail.com>
2019-07-03 14:16:59 -0500

    added class to crush wine fesitval

--- src/videos/CrushWineFestival.js
+++ src/videos/CrushWineFestival.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CrushWineFestival = () => {
     return (
         <div className="sbe-video-container">
-            <iframe title="Crush Wine Festival Expanding!" src="https://player.vimeo.com/video/344874010?title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+            <iframe title="Crush Wine Festival Expanding!" className="home-video" src="https://player.vimeo.com/video/344874010?title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
             <p><a href="https://vimeo.com/344874010">Crush Wine Festival_Expanding!</a> <br/> from <a href="https://vimeo.com/streetseatsandbeats">Streets Eats and Beats</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
         </div>
     )
