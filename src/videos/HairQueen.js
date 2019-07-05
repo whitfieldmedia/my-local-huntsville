cbce7c6dbbfd24c21391fc39f35ab4885de8bde4
Spencer <backmanspencer99@gmail.com>
2019-07-05 08:58:35 -0500

    fixed hairqueen video

--- src/videos/HairQueen.js
+++ src/videos/HairQueen.js
@@ -5,8 +5,8 @@ const HairQueen = () => {
         <div className="sbe-video-container">
             {/* <h3 className="video-header"> Hair Queen Ribbon Cutting </h3> */}
             {/* <iframe className="home-video" title="Hair Queen Beauty Ribbon Cutting" src="https://player.vimeo.com/video/306376134" frameBorder="0" allowFullScreen></iframe> */}
-            <iframe className="home-video" title="Hair Queen Beauty Ribbon Cutting" src="https://player.vimeo.com/video/344847523?title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
-            <p><a href="https://vimeo.com/344847523">CFD Research Corporation&#039;s Ribbon Cutting</a> <br/> from <a href="https://vimeo.com/streetseatsandbeats">Streets Eats and Beats</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+            <iframe className="home-video" title="Hair Queen Beauty Ribbon Cutting" src="https://player.vimeo.com/video/306376134?title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+            <p><a href="https://vimeo.com/306376134/679d3c2409">Hair Queen Beauty&#039;s  Ribbon Cutting Highlights</a> <br/> from <a href="https://vimeo.com/streetseatsandbeats">Streets Eats and Beats</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
         </div>
     )
 }
