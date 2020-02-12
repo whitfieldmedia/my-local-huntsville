be146066b0297773902f2a0e266f8da6e521a809
spencerbackman <backmanspencer99@gmail.com>
2020-02-12 09:14:53 -0600

    added valentines day newsletter

--- src/Events.js
+++ src/Events.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import newsletter from './images/february_newsletter.png';
+import newsletter from './images/newsletter_february-2.png';
 import { Helmet } from 'react-helmet';
 import './css/events.css';
 
